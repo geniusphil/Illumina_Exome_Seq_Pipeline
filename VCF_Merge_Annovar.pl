13549807293cacfd904a578d28e843b7f085532c
geniusphil <genius.philip@gmail.com>
2013-03-19 17:34:21 +0800

    vcf file merge and annotation.

---
+++ VCF_Merge_Annovar.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+#
+
+$f=$ARGV[0]; ## vcf file list
+$out=$ARGV[1]; ## out merg file
+
+open (IN,$f) || die "Can't open the file\n";
+
+@file=<IN>;
+chomp @file;
+
+close IN;
+
+# use vcf-merge
+`vcf-merge @file | bgzip -c > $out`;
+
+# use annovar convert to annovar format
+`convert2annovar.pl -format vcf4 $out > "$out".avinput`;
+
+# use annovar summarize
+`summarize_annovar.pl -buildver hg19 --verdbsnp 137 --ver1000g 1000g2012apr --veresp 6500 --genetype knowngene --outfile /mnt/mibNGS/annotation/OutputFiles/ "$out".avinput /opt/annovar_2013feb11/humandb/`;
+
