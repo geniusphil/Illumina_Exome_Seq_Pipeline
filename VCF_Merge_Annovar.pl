0cee30c00170391f6ca3d9cd2e51f2eef73306e2
Philippe <genius.philip@gmail.com>
2013-03-25 14:41:11 +0800

    Update VCF_Merge_Annovar.pl

--- VCF_Merge_Annovar.pl
+++ VCF_Merge_Annovar.pl
@@ -19,5 +19,5 @@ close IN;
 `convert2annovar.pl -format vcf4 --includeinfo $out > "$out".avinput`;
 
 # use annovar summarize
-`summarize_annovar.pl -buildver hg19 --verdbsnp 137 --ver1000g 1000g2012apr --veresp 6500 --genetype knowngene --outfile /mnt/mibNGS/ "$out".avinput /opt/annovar_2013feb11/humandb/`;
+`summarize_annovar.pl -buildver hg19 --verdbsnp 137 --ver1000g 1000g2012apr --veresp 6500 --genetype knowngene --outfile /mnt/mibNGS/ "$out".avinput /opt/annovar_2013feb21/humandb/`;
 
